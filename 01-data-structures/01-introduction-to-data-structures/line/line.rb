d0f68b726cfd3d7a2c6ba3b55be7639a02e42abc
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-05 19:57:49 -0400

    wrote line.rb. All rspec tests pass. I believe that I may revisit to style ruby correctly, at least give it a check.

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,36 @@ class Line
   end
 
   def join(person)
+    self.members.push(person)
   end
 
   def leave(person)
+    self.members.delete(person)
   end
 
   def front
+    self.members[0]
   end
 
   def middle
+    last_persons_index = self.members.length
+    self.members[(last_persons_index / 2)]
   end
 
   def back
+    self.members[-1]
   end
 
   def search(person)
+    if index(person)
+      return person
+    end
   end
 
   private
 
   def index(person)
+    self.members.index(person)
   end
 
 end
