ee136df53bd4244c079440007aca9f9cc299f1df
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-05 21:06:39 -0400

    added screen.rb that passes the screen_spec.rb correctly. However, not sure about the x, y value in the pixel vs the x, y value in the location on screen. Is the pixel x, y value an offset?

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,43 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.width = width
+    self.height = height
+    matrix = []
+
+    for j in (0..height)
+      row = []
+      for i in (0..width)
+        row.push(nil)
+      end
+      matrix.push(row)
+    end
+
+    self.matrix = matrix
+
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    inbounds(x, y) ? self.matrix[y][x] = pixel : nil
   end
 
   def at(x, y)
+    inbounds(x, y) ? self.matrix[y][x] : nil
   end
 
   private
 
   def inbounds(x, y)
+
+    if x >= self.width or x < 0
+      return false
+    elsif y >= self.height or y < 0
+      return false
+    end
+
+    true
+
   end
 
 end
