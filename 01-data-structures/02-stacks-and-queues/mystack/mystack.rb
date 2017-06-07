81f5240a379b448f491b1fa48c35cfbb14213eb6
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-07 09:40:52 -0400

    Finished mystack.rb which implements a LIFO data structure. Passes the spec

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,31 @@ class MyStack
   end
 
   def push(item)
+    if empty?
+      @stack[0] = item
+      self.top = item
+    else
+      @stack[@stack.length] = item
+      self.top = item
+    end
   end
 
   def pop
+    if !empty? 
+      popped_item = @stack.slice!(@stack.length - 1)
+      @stack.length != 0 ? self.top = @stack[@stack.length - 1] : self.top = nil
+      return popped_item
+    end
+    self.top
   end
 
   def empty?
+
+    if self.top != nil
+      return false 
+    end
+
+    true
+
   end
 end
