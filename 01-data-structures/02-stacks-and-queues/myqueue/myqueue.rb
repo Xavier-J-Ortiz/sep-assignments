3dcf1642e0afeff257ba44a32b7d256bdb2ae423
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-07 19:07:45 -0400

    done with queue.rb. I think I've learned that the bang after slice commits a change to the original array.

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,22 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[@queue.length]
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    @queue.slice!(0)
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def empty?
+    @head == nil ? true : false
   end
 end
