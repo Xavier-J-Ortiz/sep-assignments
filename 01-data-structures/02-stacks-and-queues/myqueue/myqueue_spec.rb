0de08d8bcfde1e5c9fb8dc683dd8e12d17b849c4
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-10 11:16:33 -0400

    added some tests to the queue spec. Re-arranged some tests as the order didn't seem off. That is, if you are going to use head and tail extensively in your first test, you should probably test head and tail for correct functionality before using it in a further test.

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue_spec.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue_spec.rb
@@ -6,6 +6,19 @@ RSpec.describe MyQueue, type: Class do
   let(:q) { MyQueue.new }
 
   describe "#enqueue" do
+
+    it "updates the head and tail accessors properly" do
+      q.enqueue("Rob")
+      expect(q.head).to eq "Rob"
+      expect(q.tail).to eq "Rob"
+      q.enqueue("Ben")
+      expect(q.head).to eq "Rob"
+      expect(q.tail).to eq "Ben"
+      q.enqueue("Jim")
+      expect(q.head).to eq "Rob"
+      expect(q.tail).to eq "Jim"
+    end
+
     it "adds an item to the end the queue" do
       q.enqueue("Rob")
       expect(q.head).to eq "Rob"
@@ -13,6 +26,20 @@ RSpec.describe MyQueue, type: Class do
       q.enqueue("Ben")
       expect(q.head).to eq "Rob"
       expect(q.tail).to eq "Ben"
+      q.enqueue("Jim")
+      expect(q.head).to eq "Rob"
+      expect(q.tail).to eq "Jim"
+    end
+  end
+
+  describe "#empty?" do
+    it "returns true if the queue is empty" do
+      expect(q.empty?).to eq true
+    end
+
+    it "returns false if the queue is not empty" do
+      q.enqueue("Rob")
+      expect(q.empty?).to eq false
     end
   end
 
@@ -22,26 +49,18 @@ RSpec.describe MyQueue, type: Class do
       expect(q.empty?).to eq false
       q.dequeue
       expect(q.empty?).to eq true
-    end
 
-    it "updates the head and tail accessors properly" do
       q.enqueue("Rob")
-      expect(q.head).to eq "Rob"
-      expect(q.tail).to eq "Rob"
       q.enqueue("Ben")
-      expect(q.head).to eq "Rob"
-      expect(q.tail).to eq "Ben"
-    end
-  end
-
-  describe "#empty?" do
-    it "returns true if the queue is emtpy" do
-      expect(q.empty?).to eq true
-    end
-
-    it "returns false if the queue is not empty" do
-      q.enqueue("Rob")
+      q.enqueue("Jim")
+      q.dequeue
+      expect(q.head).to eq "Ben"
+      expect(q.tail).to eq "Jim"
       expect(q.empty?).to eq false
+
+      q.dequeue
+      q.dequeue
+      expect(q.empty?).to eq true
     end
   end
 end
