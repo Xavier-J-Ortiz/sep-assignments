7ec8be2052d31358ecede9d8cb6c4a0c8c7f5c64
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-11 18:59:02 -0400

    Increased number of items in the benchmark as well as renamed some labeling. Also, added the answers to linked-lists-answers.txt

--- 01-data-structures/03-linked-lists/linked_list_benchmark.rb
+++ 01-data-structures/03-linked-lists/linked_list_benchmark.rb
@@ -1,39 +1,37 @@
 require 'benchmark'
 require_relative 'linked_list'
 
-n = 10000
+n = 100000
 test_list = LinkedList.new
 test_array = [] 
 
 Benchmark.bm do |x|
-  x.report(:L) { 
+  x.report(:Linitialize) { 
     for i in 0..n
       node = Node.new(i)
       test_list.add_to_tail(node)
     end 
   }
 
-  x.report(:A) {
+  x.report(:Ainitialize) {
     for i in 0..n
       test_array.push(i)
     end 
   } 
 
   x.report(:Lfind) {
-    test_list.locate_node_by_index(4999)
+    test_list.locate_node_by_index(49999)
   }
 
   x.report(:Afind) {
-    test_array[4999]
+    test_array[49999]
   }
 
   x.report(:Ldelete) {
-    test_list.delete_node_by_index(4999)
+    test_list.delete_node_by_index(49999)
   }
 
   x.report(:Adelete) {
-    test_array.delete(4999)
+    test_array.delete_at(49999)
   }
-
-
 end
