bfdc250ba2f344555459107ff7e31b5227062326
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-19 08:15:20 -0400

    lots of changes, need to do one more pass before submitting the checkpoint, mainly looking for fail cases, and have them fail gracefully.

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -3,121 +3,112 @@ require_relative 'node'
 class LinkedList
   attr_accessor :head
   attr_accessor :tail
+  attr_accessor :length
 
-  # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
     node.next = nil
     if @tail
       @tail.next = node
       @tail = node
+      @length += 1
     else
       @tail = node
       @head = @tail
+      @length = 1
     end
   end
 
-  # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
     if @head.next
-      temp = @head
-      temp_next = @head.next
-      while temp_next.next != nil
-        temp = temp.next
-        temp_next = temp.next
-      end
-      @tail = temp
-      temp.next = nil
+      @tail = find_node_prior(@tail, false)
+      @tail.next = nil
+      @length -= 1
     else
       @head = nil
       @tail = nil
+      @length = 0
     end
   end
 
-  # This method prints out a representation of the list.
+
   def print
-    temp = @head
-    temp_next = @head.next
-    puts temp.data
-    puts temp_next.data
-    while temp_next.next
-      temp = temp.next
-      temp_next = temp.next
-      puts temp_next.data
-    end
+    find_node_prior(@tail, true) 
   end
 
-  # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
-    if @head == node
-      if @head.next == nil
-        @tail = nil
-      end
-      @head = @head.next
+    if @head != node
+      current_node = find_node_prior(node, false)      
+      delete_next_node(current_node)
     else
-      current_node = @head
-      next_node = @head.next
-      while next_node != node
-        current_node = next_node
-        next_node = current_node.next 
-      end
-      delete_next_node(current_node, next_node)
+      self.remove_front
     end
   end
 
-  # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
     if @head
-      old_head = @head
+      node.next = @head
       @head = node
-      @head.next = old_head
+      @length += 1
     else
       @head = node
       @tail = @head
+      @length = 1
     end
   end
 
-  # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
     if @head
       @head = @head.next
       if !@head
         @tail = @head
       end
+      @length -= 1
     end
   end
-  
-  # added this due to the benchmark portion of the assignment asking for me to delete the nth element in the List.
-  # In order to do a like for like with an Array, thought appropriate to add this method.
+
   def delete_node_by_index(index)
     if index == 0
-      if @head.next != nil
-        @head = @head.next
-      else
-        @tail = nil
-        @head = nil
-      end
+      self.remove_front
     else
-      current_node = locate_node_by_index(index - 1)
-      next_node = current_node.next
-      delete_next_node(current_node, next_node)
+      delete_next_node(locate_node_by_index(index - 1))
     end
   end
 
   def  locate_node_by_index(index)
-    current_node = @head
-    next_node = @head.next
-    for i in (1..index)
-      current_node = next_node
-      next_node = current_node.next 
+    if index < @length and index >= 0
+      current_node = @head
+      next_node = @head.next
+      for i in (1..index)
+        current_node = next_node
+        next_node = current_node.next 
+      end
+      return current_node
+    else
+      return nil
     end
-    current_node
+
   end
 
-  def delete_next_node(current_node, next_node)
-    current_node.next = next_node.next
-    if next_node.next == nil
+  def find_node_prior(said_node, print)
+    node = @head
+    if print
+      puts node.data
+      puts node.next.data
+    end
+    while node.next.next != said_node.next
+      node = node.next
+      if print
+        puts node.next.data
+      end
+    end
+    node 
+  end
+
+  def delete_next_node(current_node)
+    if current_node.next.next == nil
       @tail = current_node
     end
-    next_node.next = nil
+    current_node.next = current_node.next.next
+    @length -= 1
   end
 end
--- 01-data-structures/03-linked-lists/linked_list_spec.rb
+++ 01-data-structures/03-linked-lists/linked_list_spec.rb
@@ -7,123 +7,250 @@ RSpec.describe LinkedList, type: Class do
   let(:n1) { Node.new("Rob") }
   let(:n2) { Node.new("Ben") }
   let(:n3) { Node.new("Mike") }
-  let(:llist) { LinkedList.new }
+  let(:linked_list) { LinkedList.new }
 
   describe "#add_to_tail" do
     it "adds a Node to the tail" do
-      llist.add_to_tail(n1)
-      expect(llist.tail).to eq n1
-      llist.add_to_tail(n2)
-      expect(llist.tail).to eq n2
+      linked_list.add_to_tail(n1)
+      expect(linked_list.tail).to eq n1
+      expect(linked_list.length).to eq 1
+      linked_list.add_to_tail(n2)
+      expect(linked_list.tail).to eq n2
+      expect(linked_list.length).to eq 2
     end
   end
 
   describe "#remove_tail" do
     it "removes nodes from the tail" do
-      llist.add_to_tail(n1)
-      llist.add_to_tail(n2)
-      expect(llist.tail).to eq n2
-      llist.remove_tail
-      expect(llist.tail).to eq n1
-      llist.remove_tail
-      expect(llist.tail).to eq nil
+      linked_list.add_to_tail(n1)
+      expect(linked_list.length).to eq 1
+      linked_list.add_to_tail(n2)
+      expect(linked_list.length).to eq 2
+      expect(linked_list.tail).to eq n2
+      linked_list.remove_tail
+      expect(linked_list.length).to eq 1
+      expect(linked_list.tail).to eq n1
+      linked_list.remove_tail
+      expect(linked_list.length).to eq 0
+      expect(linked_list.tail).to eq nil
     end
   end
 
   describe "#print" do
     before do
-      llist.add_to_tail(n1)
-      llist.add_to_tail(n2)
+      linked_list.add_to_tail(n1)
+      expect(linked_list.length).to eq 1
+      linked_list.add_to_tail(n2)
+      expect(linked_list.length).to eq 2
+      linked_list.add_to_tail(n3)
+      expect(linked_list.length).to eq 3
     end
-
-    specify { expect { llist.print }.to output("Rob\nBen\n").to_stdout }
+    specify { expect { linked_list.print }.to output("Rob\nBen\nMike\n").to_stdout }
   end
 
   describe "#delete" do
     before do
-      llist.add_to_tail(n1)
-      llist.add_to_tail(n2)
-      llist.add_to_tail(n3)
+      linked_list.add_to_tail(n1)
+      linked_list.add_to_tail(n2)
+      linked_list.add_to_tail(n3)
     end
 
     it "removes the head of a list properly" do
-      llist.delete(n1)
-      expect(llist.head).to eq n2
-      expect(llist.head.next).to eq n3
-      expect(llist.tail).to eq n3
+      expect(linked_list.length).to eq 3
+      linked_list.delete(n1)
+      expect(linked_list.head).to eq n2
+      expect(linked_list.head.next).to eq n3
+      expect(linked_list.tail).to eq n3
+      expect(linked_list.length).to eq 2
+      linked_list.delete(n2)
+      expect(linked_list.head).to eq n3
+      expect(linked_list.head.next).to eq nil
+      expect(linked_list.tail).to eq n3
+      expect(linked_list.length).to eq 1
+      linked_list.delete(n3)
+      expect(linked_list.head).to eq nil 
+      expect(linked_list.tail).to eq nil
+      expect(linked_list.length).to eq 0
     end
 
     it "removes the middle element of a list properly" do
-      llist.delete(n2)
-      expect(llist.head).to eq n1
-      expect(llist.head.next).to eq n3
-      expect(llist.tail).to eq n3
+      linked_list.delete(n2)
+      expect(linked_list.length).to eq 2
+      expect(linked_list.head).to eq n1
+      expect(linked_list.head.next).to eq n3
+      expect(linked_list.tail).to eq n3
+      linked_list.delete(n3)
+      expect(linked_list.head).to eq n1
+      expect(linked_list.head.next).to eq nil
+      expect(linked_list.tail).to eq n1
+      expect(linked_list.length).to eq 1
+      linked_list.delete(n1)
+      expect(linked_list.head).to eq nil 
+      expect(linked_list.tail).to eq nil
+      expect(linked_list.length).to eq 0
     end
 
     it "removes the last element of a list properly" do
-      llist.delete(n3)
-      expect(llist.head).to eq n1
-      expect(llist.head.next).to eq n2
-      expect(llist.tail).to eq n2
+      linked_list.delete(n3)
+      expect(linked_list.length).to eq 2
+      expect(linked_list.head).to eq n1
+      expect(linked_list.head.next).to eq n2
+      expect(linked_list.tail).to eq n2
+      linked_list.delete(n1)
+      expect(linked_list.head).to eq n2
+      expect(linked_list.head.next).to eq nil
+      expect(linked_list.tail).to eq n2
+      expect(linked_list.length).to eq 1
+      linked_list.delete(n2)
+      expect(linked_list.head).to eq nil 
+      expect(linked_list.tail).to eq nil
+      expect(linked_list.length).to eq 0
     end
   end
 
   describe "#add_to_front" do
     it "adds the node to the front of the linked list" do
-      llist.add_to_front(n1)
-      expect(llist.head).to eq n1
-      llist.add_to_front(n2)
-      expect(llist.head).to eq n2
+      linked_list.add_to_front(n1)
+      expect(linked_list.length).to eq 1
+      expect(linked_list.head).to eq n1
+      linked_list.add_to_front(n2)
+      expect(linked_list.length).to eq 2
+      expect(linked_list.head).to eq n2
+      linked_list.add_to_front(n3)
+      expect(linked_list.length).to eq 3
+      expect(linked_list.head).to eq n3
+      expect(linked_list.tail).to eq n1
     end
   end
 
   describe "#remove_front" do
     it "removes the node to the front of the linked list" do
-      llist.add_to_front(n1)
-      expect(llist.head).to eq n1
-      llist.add_to_front(n2)
-      expect(llist.head).to eq n2
-      llist.remove_front
-      expect(llist.head).to eq n1
-      llist.remove_front
-      expect(llist.head).to eq nil
+      linked_list.add_to_front(n1)
+      expect(linked_list.length).to eq 1
+      expect(linked_list.head).to eq n1
+      linked_list.add_to_front(n2)
+      expect(linked_list.length).to eq 2
+      expect(linked_list.head).to eq n2
+      linked_list.remove_front
+      expect(linked_list.length).to eq 1
+      expect(linked_list.head).to eq n1
+      linked_list.remove_front
+      expect(linked_list.length).to eq 0
+      expect(linked_list.head).to eq nil
     end
   end
 
   describe "#locate_node_by_index" do
     before do
-      llist.add_to_tail(n1)
-      llist.add_to_tail(n2)
-      llist.add_to_tail(n3)
+      linked_list.add_to_tail(n1)
+      linked_list.add_to_tail(n2)
+      linked_list.add_to_tail(n3)
     end
 
     it "locates nodes by index" do
-      expect(llist.locate_node_by_index(0).data).to eq "Rob"
-      expect(llist.locate_node_by_index(1).data).to eq "Ben"
-      expect(llist.locate_node_by_index(2).data).to eq "Mike"
-      llist.delete(n2)
-      expect(llist.locate_node_by_index(0).data).to eq "Rob"
-      expect(llist.locate_node_by_index(1).data).to eq "Mike"
-    end
+      expect(linked_list.length).to eq 3
+      expect(linked_list.locate_node_by_index(0).data).to eq "Rob"
+      expect(linked_list.locate_node_by_index(1).data).to eq "Ben"
+      expect(linked_list.locate_node_by_index(2).data).to eq "Mike"
+      expect(linked_list.locate_node_by_index(-2)).to eq nil
+      linked_list.delete(n2)
+      expect(linked_list.length).to eq 2
+      expect(linked_list.locate_node_by_index(0).data).to eq "Rob"
+      expect(linked_list.locate_node_by_index(1).data).to eq "Mike"
+      linked_list.delete(n3)
+      expect(linked_list.length).to eq 1
+      expect(linked_list.locate_node_by_index(0).data).to eq "Rob"
+      linked_list.delete(n1)
+      expect(linked_list.length).to eq 0
+      expect(linked_list.locate_node_by_index(0)).to eq nil
 
-    describe "#delete_node_by_index" do
-      before do
-        llist.add_to_tail(n1)
-        llist.add_to_tail(n2)
-        llist.add_to_tail(n3)
-      end
-
-      it "deletes nodes by index" do
-        expect(llist.locate_node_by_index(0).data).to eq "Rob"
-        expect(llist.locate_node_by_index(1).data).to eq "Ben"
-        expect(llist.locate_node_by_index(2).data).to eq "Mike"
-        llist.delete_node_by_index(1) 
-        expect(llist.locate_node_by_index(0).data).to eq "Rob"
-        expect(llist.locate_node_by_index(1).data).to eq "Mike"
-        llist.delete_node_by_index(0) 
-        expect(llist.locate_node_by_index(0).data).to eq "Mike" 
-      end
     end
   end
+  describe "#delete_node_by_index" do
+    before do
+      linked_list.add_to_tail(n1)
+      linked_list.add_to_tail(n2)
+      linked_list.add_to_tail(n3)
+    end
+
+    it "deletes the node at the given index" do
+      expect(linked_list.length).to eq 3
+      expect(linked_list.locate_node_by_index(0).data).to eq "Rob"
+      expect(linked_list.locate_node_by_index(1).data).to eq "Ben"
+      expect(linked_list.locate_node_by_index(2).data).to eq "Mike"
+      linked_list.delete_node_by_index(1) 
+      expect(linked_list.length).to eq 2
+      expect(linked_list.locate_node_by_index(0).data).to eq "Rob"
+      expect(linked_list.locate_node_by_index(1).data).to eq "Mike"
+      linked_list.delete_node_by_index(0) 
+      expect(linked_list.length).to eq 1
+      expect(linked_list.locate_node_by_index(0).data).to eq "Mike" 
+      linked_list.delete_node_by_index(0) 
+      expect(linked_list.length).to eq 0
+      expect(linked_list.locate_node_by_index(0)).to eq nil 
+    end
+  end
+
+  describe "#locate_node_by_index" do
+    before do
+      linked_list.add_to_tail(n1)
+      linked_list.add_to_tail(n2)
+      linked_list.add_to_tail(n3)
+    end
+
+    it "checks for nil return on trying to locate an index that is out of bounds" do
+      expect(linked_list.length).to eq 3
+      expect(linked_list.locate_node_by_index(0).data).to eq "Rob"
+      expect(linked_list.locate_node_by_index(1).data).to eq "Ben"
+      expect(linked_list.locate_node_by_index(2).data).to eq "Mike"
+      expect(linked_list.locate_node_by_index(5)).to eq nil
+      linked_list.delete_node_by_index(1) 
+      expect(linked_list.length).to eq 2
+      expect(linked_list.locate_node_by_index(0).data).to eq "Rob"
+      expect(linked_list.locate_node_by_index(1).data).to eq "Mike"
+      expect(linked_list.locate_node_by_index(2)).to eq nil
+      linked_list.delete_node_by_index(0) 
+      expect(linked_list.length).to eq 1
+      expect(linked_list.locate_node_by_index(0).data).to eq "Mike" 
+    end
+  end
+
+describe "#find_node_prior" do
+    before do
+      linked_list.add_to_tail(n1)
+      linked_list.add_to_tail(n2)
+      linked_list.add_to_tail(n3)
+    end
+
+    it "" do
+      expect(linked_list.length).to eq 3
+      expect(linked_list.find_node_prior(n3, false).data).to eq "Ben"
+      expect(linked_list.find_node_prior(n2, false).data).to eq "Rob"
+    end
+  end
+
+
+  describe "#delete_next_node" do
+    before do
+      linked_list.add_to_tail(n1)
+      linked_list.add_to_tail(n2)
+      linked_list.add_to_tail(n3)
+    end
+
+    it "checks for nil return on trying to locate an index that is out of bounds" do
+      expect(linked_list.length).to eq 3
+      linked_list.delete_next_node(n1)
+      expect(linked_list.length).to eq 2
+      expect(linked_list.locate_node_by_index(0).data).to eq "Rob" 
+      expect(linked_list.locate_node_by_index(1).data).to eq "Mike" 
+      expect(linked_list.locate_node_by_index(2)).to eq nil 
+      linked_list.delete_next_node(n2)
+      expect(linked_list.length).to eq 1
+      expect(linked_list.locate_node_by_index(0).data).to eq "Rob" 
+      expect(linked_list.locate_node_by_index(2)).to eq nil 
+    end
+  end
+
+
 end
 
