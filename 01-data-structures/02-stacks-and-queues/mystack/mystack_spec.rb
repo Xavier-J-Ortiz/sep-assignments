194078acaa7670619b01afc2ffdc06072cde578d
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-22 22:03:19 -0400

    chunked down the tests. I think it's looking better.

--- 01-data-structures/02-stacks-and-queues/mystack/mystack_spec.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack_spec.rb
@@ -5,99 +5,69 @@ require_relative 'mystack'
 RSpec.describe MyStack, type: Class do
   let(:stack) { MyStack.new }
 
-  describe "#push" do
-    it "pushes a single item and checks the top of the stack stack height" do
-      stack.push("Rob")
-      expect(stack.top).to eq "Rob"
-    end
+  def pusher
+    stack.push("Rob")
+    stack.push("Ben")
+    stack.push("Tim")
+    stack.push("Jinhai")
+    stack.push("Frank")
+  end
 
-    it "pushes 5 items and checks the top of the stack and stack height" do
+  describe "#push" do
+    it "pushes a single item and checks the top of the stack" do
       stack.push("Rob")
       expect(stack.top).to eq "Rob"
       expect(stack.stack_length).to eq 1
+    end
 
+    it "pushes 2 items and checks the top of the stack" do
+      stack.push("Rob")
       stack.push("Ben")
       expect(stack.top).to eq "Ben"
       expect(stack.stack_length).to eq 2
+    end
 
+    it "pushes a single item and checks the top of the stack" do
+      stack.push("Rob")
+      stack.push("Ben")
       stack.push("Tim")
       expect(stack.top).to eq "Tim"
       expect(stack.stack_length).to eq 3
-
-      stack.push("Jinhai")
-      expect(stack.top).to eq "Jinhai"
-      expect(stack.stack_length).to eq 4
-
-      stack.push("Frank")
-      expect(stack.top).to eq "Frank"
-      expect(stack.stack_length).to eq 5
     end
   end
 
   describe "#pop" do
-
     before do
-      stack.push("Rob")
-      stack.push("Ben")
-      stack.push("Tim")
-      stack.push("Jinhai")
-      stack.push("Frank")
+      pusher
     end
 
     it "pops an item off the top of the stack and returns it" do
       item = stack.pop
       expect(item).to eq "Frank"
-
-      item = stack.pop
-      expect(item).to eq "Jinhai"
-
-      item = stack.pop
-      expect(item).to eq "Tim"
-
-      item = stack.pop
-      expect(item).to eq "Ben"
-
-      item = stack.pop
-      expect(item).to eq "Rob"
+      expect(stack.top).to eq "Jinhai"
+      expect(stack.stack_length).to eq 4
     end
   end
 
   describe "#empty?" do
-
     context "when stack is initialized" do
-
       it "returns true" do
         expect(stack.empty?).to eq true
       end
     end
 
     context "when stack has multiple items loaded" do
-
       before do
-        stack.push("Rob")
-        stack.push("Ben")
-        stack.push("Tim")
         stack.push("Jinhai")
-        stack.push("Frank")
       end
 
-      it "returns false if stack is not empty, returns true if stack is empty" do
-        expect(stack.empty?).to eq false
-
-        stack.pop
-        expect(stack.empty?).to eq false
-
-        stack.pop
-        expect(stack.empty?).to eq false
-
-        stack.pop
-        expect(stack.empty?).to eq false
-
-        stack.pop
-        expect(stack.empty?).to eq false
-
+      it "returns true if stack is empty" do
         stack.pop
         expect(stack.empty?).to eq true 
+      end
+
+      it "returns false if stack is not empty" do
+        expect(stack.empty?).to eq false
       end
     end
   end
