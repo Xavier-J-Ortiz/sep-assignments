ad3fb678032aecf35c796c7f38d4cef00d938a7f
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-22 22:03:19 -0400

    chunked down the tests. I think it's looking better.

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue_spec.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue_spec.rb
@@ -6,26 +6,15 @@ RSpec.describe MyQueue, type: Class do
   let(:q) { MyQueue.new }
 
   describe "#enqueue" do
-
-    it "updates the head and tail accessors properly" do
+    it "enqueues strings to the queue, verifies with head and tail" do
       q.enqueue("Rob")
       expect(q.head).to eq "Rob"
       expect(q.tail).to eq "Rob"
-      q.enqueue("Ben")
-      expect(q.head).to eq "Rob"
-      expect(q.tail).to eq "Ben"
-      q.enqueue("Jim")
-      expect(q.head).to eq "Rob"
-      expect(q.tail).to eq "Jim"
-    end
 
-    it "adds an item to the end the queue" do
-      q.enqueue("Rob")
-      expect(q.head).to eq "Rob"
-      expect(q.tail).to eq "Rob"
       q.enqueue("Ben")
       expect(q.head).to eq "Rob"
       expect(q.tail).to eq "Ben"
+      
       q.enqueue("Jim")
       expect(q.head).to eq "Rob"
       expect(q.tail).to eq "Jim"
@@ -44,33 +33,30 @@ RSpec.describe MyQueue, type: Class do
   end
 
   describe "#dequeue" do
-    it "removes an item from the front of the queue that has one element" do
-      q.enqueue("Rob")
-      expect(q.empty?).to eq false
-      q.dequeue
-      expect(q.empty?).to eq true
-    end
-
-    it "removes an item from the front of the queue with 3 elements" do
+    before do
       q.enqueue("Rob")
       q.enqueue("Ben")
       q.enqueue("Jim")
+    end
+
+    it "removes an item from the front of the queue" do
       q.dequeue
       expect(q.head).to eq "Ben"
       expect(q.tail).to eq "Jim"
       expect(q.empty?).to eq false
     end
-    
-    it "removes an item from the front of the queue with 2 elements" do
-      q.enqueue("Ben")
-      q.enqueue("Jim")
+
+    it "removes items from the front of the queue leaving one element" do
+      q.dequeue
       q.dequeue
       expect(q.head).to eq "Jim"
       expect(q.tail).to eq "Jim"
       expect(q.empty?).to eq false
     end
-    it "removes an item from the front of the queue with 1 element" do
-      q.enqueue("Jim")
+
+    it "removes all items from the queue" do
+      q.dequeue
+      q.dequeue
       q.dequeue
       expect(q.head).to eq nil
       expect(q.tail).to eq nil
