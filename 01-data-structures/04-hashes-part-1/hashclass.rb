3e17df2f502dee1d473ebaddd6a7a6c2aaf1f9c5
xortiz <xavier.ortiz.ch@gmail.com>
2017-06-30 19:39:22 -0400

    changed some wording in some tests, also added the answer to the .txt file

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,40 +1,40 @@
 class HashClass
   require 'murmurhash3'
+
   def initialize(size)
     @items = Array.new(size)
     @keys = Array.new(size)
   end
 
   def []=(key, value)
-    true_index = index(key, self.size)
-    if @items[true_index].nil? 
-      @keys[true_index] = key
-      @items[true_index] = value
-    elsif (value != self[key] && !(@keys[true_index] == key))
-      while !@items[true_index].nil? 
+    true_index = index(key, self.get_size)
+    if @items[true_index].nil? or (value != self[key] && @keys[true_index] == key)
+      if (value != self[key] && @keys[true_index] == key)
         self.resize
-        true_index = index(key, self.size)
+        true_index = index(key, self.get_size)
       end
       @keys[true_index] = key
       @items[true_index] = value
-    elsif (value != self[key] && (@keys[true_index] == key))
-      self.resize
-      true_index = index(key, self.size)
-      @keys[true_index] = key
-      @items[true_index] = value
+      self.print_status
+    elsif (value != self[key] && !(@keys[true_index] == key))
+      while !@items[true_index].nil? 
+        self.resize
+        true_index = index(key, self.get_size)
+      end
+      self[key] = value
     end
   end
 
   def [](key)
-    true_index = index(key, self.size)
+    true_index = index(key, self.get_size)
     @items[true_index]
   end
 
   def resize
     old_items = @items.clone
     old_keys = @keys.clone
-    @items = Array.new(self.size * 2)
-    @keys = Array.new(self.size)
+    @items = Array.new(self.get_size * 2)
+    @keys = Array.new(self.get_size)
     (0...old_keys.length).each do |old_index|
       if (old_keys[old_index] && old_items[old_index])
         self[old_keys[old_index]] = old_items[old_index]
@@ -46,8 +46,20 @@ class HashClass
     MurmurHash3::V32.str_hash(key.to_s, size) % size
   end
 
-  # Simple method to return the number of items in the hash
-  def size
+  def get_size
     @items.length
+  end
+
+  def print_status
+    puts
+    puts "the array size is: " + self.get_size.to_s
+    puts "the array state is :"
+    puts "{"
+    for element in @keys do
+      if !element.nil?
+        puts "\t'" + element.to_s + "' : '" + self[element].to_s + "',"
+      end
+    end
+    puts "}"
   end
 end
