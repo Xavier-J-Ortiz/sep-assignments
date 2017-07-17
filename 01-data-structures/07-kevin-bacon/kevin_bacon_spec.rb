27ff2008465a75322889a1e7e597c39f38ef9021
xortiz <xavier.ortiz.ch@gmail.com>
2017-07-16 20:14:28 -0400

    separated specs for node and movie, refactored a bit more, cleaned up comments, debugging  etc.

--- 01-data-structures/07-kevin-bacon/kevin_bacon_spec.rb
+++ 01-data-structures/07-kevin-bacon/kevin_bacon_spec.rb
@@ -4,86 +4,47 @@ require_relative 'node'
 require_relative 'movie'
 require_relative 'kb_finder'
 
-RSpec.describe Node, type: Class do
+RSpec.describe "kb_finder" do
 
-  let(:bacon) {Node.new("Kevin Bacon")}
-  let(:mcavoy) {Node.new("James McAvoy")}
-  let(:fassbender) {Node.new("Michael Fassbender")}
-  let(:jlaw) {Node.new("Jennifer Lawrence")}
-  let(:lhemsworth) {Node.new("Liam Hemsworth")}
-  let(:hutcherson) {Node.new("Josh Hutcherson")}
-  let(:freeman) {Node.new("Morgan Freeman")}
-  let(:pitt) {Node.new("Brad Pitt")}
-  let(:paltrow) {Node.new("Gwyneth Paltrow")}
-  let(:blanchett) {Node.new("Cate Blanchett")}
-  let(:swinton) {Node.new("Tilda Swinton")}
-  let(:wood) {Node.new("Elija Wood")}
-  let(:mckellen) {Node.new("Ian McKellen")}
-  let(:bloom) {Node.new("Kevin Bacon")}
-  let(:stallone) {Node.new("Sylvester Stallone")}
-  let(:schwarzenegger) {Node.new("Arnold Schwarzenegger")}
-  let(:couture) {Node.new("Randy Couture")}
-  let(:hamilton) {Node.new("Linda Hamilton")}
-  let(:furlong) {Node.new("Edward Furlong")}
-  let(:norton) {Node.new("Edward Norton")}
-  let(:gere) {Node.new("Richard Gere")}
-  let(:connery) {Node.new("Sean Connery")}
-  let(:ormond) {Node.new("Julia Ormond")}
-  let(:tierney) {Node.new("Maura Tierney")}
-
-  first_knight = nil
-  primal_fear = nil
-  american_history_x = nil
-  terminator_2 = nil
-  expendibles_2 = nil
-  expendibles = nil
-  hunger = nil
-  first_class = nil
-  se7en = nil
-  button = nil
-  lotr = nil
-
-  describe "#node" do
-    it "creates brand new node correctly" do
-      expect(bacon.name).to eq "Kevin Bacon"
-    end
-  end
-
-  describe "#movie" do
-    before do
-      first_knight = Movie.new("First Knight", [ormond, gere, connery])
-      primal_fear = Movie.new("Primal Fear", [gere, norton, tierney])
-      american_history_x = Movie.new("American History X", [furlong, norton])
-      terminator_2 = Movie.new("Terminator 2: Judgement Day", [schwarzenegger, hamilton, furlong])
-      expendibles_2 = Movie.new("The Expendibles 2", [lhemsworth, couture, stallone])
-      expendibles = Movie.new("The Expendibles", [stallone, schwarzenegger])
-      hunger = Movie.new("The Hunger Games", [jlaw, lhemsworth, hutcherson])
-      first_class = Movie.new("X-Men: First Class", [mcavoy, fassbender, jlaw, bacon])
-      se7en = Movie.new("Se7en", [pitt, freeman, paltrow])
-      button = Movie.new("The Curious Case of Benjamin Button", [pitt, blanchett, swinton])
-      lotr = Movie.new("The Lord of the Rings: The Fellowship of the Ring", [wood, mckellen, bloom])
-    end
-    it "movie title associated correctly" do
-      expect(expendibles.title).to eq "The Expendibles"
-    end
-    it "movie actors associated correctly" do 
-      expect(expendibles.actors).to eq [stallone, schwarzenegger]
-    end
-  end
+  let(:bacon) { Node.new("Kevin Bacon") }
+  let(:mcavoy) { Node.new("James McAvoy") }
+  let(:fassbender) { Node.new("Michael Fassbender") }
+  let(:jlaw) { Node.new("Jennifer Lawrence") }
+  let(:lhemsworth) { Node.new("Liam Hemsworth") }
+  let(:hutcherson) { Node.new("Josh Hutcherson") }
+  let(:freeman) { Node.new("Morgan Freeman") }
+  let(:pitt) { Node.new("Brad Pitt") }
+  let(:paltrow) { Node.new("Gwyneth Paltrow") }
+  let(:blanchett) { Node.new("Cate Blanchett") }
+  let(:swinton) { Node.new("Tilda Swinton") }
+  let(:wood) { Node.new("Elija Wood") }
+  let(:mckellen) { Node.new("Ian McKellen") }
+  let(:bloom) { Node.new("Kevin Bacon") }
+  let(:stallone) { Node.new("Sylvester Stallone") }
+  let(:schwarzenegger) { Node.new("Arnold Schwarzenegger") }
+  let(:couture) { Node.new("Randy Couture") }
+  let(:hamilton) { Node.new("Linda Hamilton") }
+  let(:furlong) { Node.new("Edward Furlong") }
+  let(:norton) { Node.new("Edward Norton") }
+  let(:gere) { Node.new("Richard Gere") }
+  let(:connery) { Node.new("Sean Connery") }
+  let(:ormond) { Node.new("Julia Ormond") }
+  let(:tierney) { Node.new("Maura Tierney") }
+  let(:lithgow) { Node.new("John Lithgow") }
 
   describe "#find_kevin_bacon" do
     before do
-      first_knight = Movie.new("First Knight", [ormond, gere, connery])
-      primal_fear = Movie.new("Primal Fear", [gere, norton, tierney])
-      american_history_x = Movie.new("American History X", [furlong, norton])
-      terminator_2 = Movie.new("Terminator 2: Judgement Day", [schwarzenegger, hamilton, furlong])
-      expendibles_2 = Movie.new("The Expendibles 2", [lhemsworth, couture, stallone])
-      expendibles = Movie.new("The Expendibles", [stallone, schwarzenegger])
-      hunger = Movie.new("The Hunger Games", [jlaw, lhemsworth, hutcherson])
-      first_class = Movie.new("X-Men: First Class", [mcavoy, fassbender, jlaw, bacon])
-      se7en = Movie.new("Se7en", [pitt, freeman, paltrow])
-      button = Movie.new("The Curious Case of Benjamin Button", [pitt, blanchett, swinton])
-      lotr = Movie.new("The Lord of the Rings: The Fellowship of the Ring", [wood, mckellen, bloom])
+      @first_knight = Movie.new("First Knight", [ormond, gere, connery])
+      @primal_fear = Movie.new("Primal Fear", [gere, norton, tierney])
+      @american_history_x = Movie.new("American History X", [furlong, norton])
+      @terminator_2 = Movie.new("Terminator 2: Judgement Day", [schwarzenegger, hamilton, furlong])
+      @expendibles_2 = Movie.new("The Expendibles 2", [lhemsworth, couture, stallone])
+      @expendibles = Movie.new("The Expendibles", [stallone, schwarzenegger])
+      @hunger = Movie.new("The Hunger Games", [jlaw, lhemsworth, hutcherson])
+      @first_class = Movie.new("X-Men: First Class", [mcavoy, fassbender, jlaw, bacon])
+      @se7en = Movie.new("Se7en", [pitt, freeman, paltrow])
+      @button = Movie.new("The Curious Case of Benjamin Button", [pitt, blanchett, swinton])
+      @lotr = Movie.new("The Lord of the Rings: The Fellowship of the Ring", [wood, mckellen, bloom])
     end
 
     it "returns an empty array if the node is Kevin Bacon" do
@@ -96,6 +57,26 @@ RSpec.describe Node, type: Class do
 
     it "returns nil if distance to kevin bacon is greater than 6" do
       expect(find_kevin_bacon(tierney)).to eq nil
+    end
+  end
+
+  describe "#trace_path" do
+    context "When Kevin Bacon is reached via John Lithgow"
+    before do
+      @path = {}
+      @path["Mike Meyers"] = {:distance =>  0,
+                              :previous_node => nil, 
+                              :encountered_movie => nil}
+      @path["John Lithgow"] = {:distance =>  1,
+                               :previous_node => @path["Mike Meyers"],
+                               :encountered_movie => "Shrek"}
+      @path["Kevin Bacon"] = {:distance =>  2, 
+                              :previous_node => @path["John Lithgow"], 
+                              :encountered_movie => "Footloose"}
+    end
+
+    it "returns degrees of separation between original actor and Kevin Bacon" do
+      expect(trace_path("Footloose", @path, lithgow)).to eq ["Shrek", "Footloose"]
     end
   end
 end
