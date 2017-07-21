0a2996372541ed23f734ce89e1738b847eb6fa4a
xortiz <xavier.ortiz.ch@gmail.com>
2017-07-21 18:06:03 -0400

    cleaned up some names, methods, refactored some returns.

--- 01-data-structures/07-kevin-bacon/kb_finder.rb
+++ 01-data-structures/07-kevin-bacon/kb_finder.rb
@@ -1,49 +1,50 @@
 require_relative 'node'
 require_relative 'movie'
 def find_kevin_bacon(node)
+
   if node.name == "Kevin Bacon"
     return []
   end
 
-  queue = [node]
+  bfs_actor_queue = [node]
   path = {}
   current_actor = node
+  max_steps = 5
   path[current_actor.name] = {:distance => 0, 
                               :previous_node => nil, 
                               :encountered_movie => nil}
-
-  while (!queue.empty? and path[current_actor.name][:distance] < 5)
-    current_actor = queue.shift
-    knows_kevin_bacon = current_actor_knows_kevin_bacon(current_actor, queue, path)
-    if knows_kevin_bacon[0]
-      return knows_kevin_bacon[1]
+  while (!bfs_actor_queue.empty? and path[current_actor.name][:distance] < max_steps)
+    current_actor = bfs_actor_queue.shift
+    knows_kevin_bacon = does_current_actor_know_kevin_bacon?(current_actor, bfs_actor_queue, path)
+    if knows_kevin_bacon
+      return knows_kevin_bacon
     end
   end
   nil
 end
 
-def current_actor_knows_kevin_bacon(current_actor, queue, path)
+def does_current_actor_know_kevin_bacon?(current_actor, bfs_actor_queue, path)
   current_actor.film_actor_hash.each do |current_movie, actors|
-    knows_kevin_bacon = current_actor_worked_with_kevin_bacon_in_current_movie(current_movie, current_actor, actors, queue, path)
-    if knows_kevin_bacon[0]
+    knows_kevin_bacon = worked_with_kevin_here?(current_movie, current_actor, actors, bfs_actor_queue, path)
+    if knows_kevin_bacon
       return knows_kevin_bacon
     end
   end
-  return [false, false]
+  return false
 end
 
-def current_actor_worked_with_kevin_bacon_in_current_movie(current_movie, current_actor, actors, queue, path)
+def worked_with_kevin_here?(current_movie, current_actor, actors, bfs_actor_queue, path)
   for actor in actors
-    if actor.name != "Kevin Bacon" and !path.has_key?(actor.name) 
-      queue.push(actor)
+    if (actor.name != "Kevin Bacon" and !path.has_key? actor.name) 
+      bfs_actor_queue.push actor 
       path[actor.name] = {:distance => path[current_actor.name][:distance] + 1, 
                           :previous_node => path[current_actor.name], 
                           :encountered_movie => current_movie}
     elsif actor.name == "Kevin Bacon"
-      return [true, trace_movies_to_kevin(current_movie, path, current_actor)]
+      return trace_movies_to_kevin(current_movie, path, current_actor)
     end
   end
-  return [false, false]
+  return false
 end
 
 def trace_movies_to_kevin(current_movie, path, current_actor)
